719467f7221a531cfd8055fa416d73f064dd52fc
nemo_home888@hotmail.com <aztech_102102>
2019-10-21 22:17:23 +0800

    dice

---
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.testing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_btn)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        /*Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()*/
+        val resultText: TextView = findViewById(R.id.roll_int)
+        val diceImage: ImageView = findViewById(R.id.dice_img)
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        resultText.text = randomInt.toString()
+
+    }
+}
